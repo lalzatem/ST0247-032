bb79674f8b2d3fe54b5d95e10d7a1019f99acd80
Dalzatec1 <47001384+Dalzatec1@users.noreply.github.com>
2019-02-01 23:27:06 -0500

    Update nosecomollamaresto.java

--- talleres/taller02/nosecomollamaresto.java
+++ talleres/taller02/nosecomollamaresto.java
@@ -78,17 +78,5 @@ public class nosecomollamaresto{
             }
     }
 
-        public static void main(String[] args){
-
-            nosecomollamaresto hola=new nosecomollamaresto();
-            System.out.println("COMBINATORIA");
-            hola.conjuntoPotencia("abc");
-            System.out.println("");
-    
-            System.out.println("PERMUTACION");
-            ArrayList<String> arreglo = permutaciones("abcd");
-            for(String s : arreglo)
-                System.out.println(s + " " + AdvancedEncryptionStandard.desencriptarArchivo(s));
-    }
-
+      
 }

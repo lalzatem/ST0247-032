d3f163e2915e9a73209cc945cf8eea646ff157ee
Dalzatec1 <47001384+Dalzatec1@users.noreply.github.com>
2019-02-01 23:27:31 -0500

    Update Main.java

--- talleres/taller02/Main.java
+++ talleres/taller02/Main.java
@@ -1,9 +1,15 @@
 public class Main{
-    public static void main(String[] args){
-        nosecomollamaresto hola=new nosecomollamaresto();
+  public static void main(String[] args){
 
-        hola.conjuntoPotencia("abc");
-        System.out.println("");
-        hola.permutaciones("abcd");
+            nosecomollamaresto hola=new nosecomollamaresto();
+            System.out.println("COMBINATORIA");
+            hola.conjuntoPotencia("abc");
+            System.out.println("");
+    
+            System.out.println("PERMUTACION");
+            ArrayList<String> arreglo = permutaciones("abcd");
+            for(String s : arreglo)
+                System.out.println(s + " " + AdvancedEncryptionStandard.desencriptarArchivo(s));
     }
+
 }

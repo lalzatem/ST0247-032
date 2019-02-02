96194b687390a86cc49cf6e132867c7fe106c285
Dalzatec1 <47001384+Dalzatec1@users.noreply.github.com>
2019-02-01 23:14:53 -0500

    Create  Main.java

---
+++ talleres/taller02/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        nosecomollamaresto hola=new nosecomollamaresto();
+
+        hola.conjuntoPotencia("abc");
+        System.out.println("");
+        hola.permutaciones("abcd");
+    }
+}

045fcf9104d98c63dfd5e3d98f4aa85f9fd5d00e
Dalzatec1 <47001384+Dalzatec1@users.noreply.github.com>
2019-03-17 20:57:05 -0500

    Add files via upload

---
+++ laboratorios/lab03/ejercicioEnLinea/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.HashMap;
+
+/**
+ * Prueba la implementacion de los metodos en la clase punto2_1.
+ * 
+
+ * 
+
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        punto2_1 hola=new punto2_1();
+        hola.nums();
+    }
+
+}
+
